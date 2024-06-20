0166712fff738b0ff8d3604bcfeeb01f846368ef
Mike Lloyd <mike.lloyd@teradata.com>
2024-06-20 16:59:08 -0400

    Create config dir on init

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,9 @@ use anyhow::Result;
 pub fn default_config_dir() -> Result<PathBuf> {
     let xdg_dirs = xdg::BaseDirectories::new()?;
     let config_dir = xdg_dirs.get_config_home();
+    let rudric_config_dir = config_dir.join("rudric");
 
-    Ok(config_dir.join("rudric"))
+    std::fs::create_dir_all(rudric_config_dir.clone())?;
+
+    Ok(rudric_config_dir)
 }
